5224bb8324681b0cb91f19c9afa38b3c422c1364
Darvetra <darvetra13@gmail.com>
2020-05-27 22:04:39 +0300

    финальные правки

--- source/js/app.js
+++ source/js/app.js
@@ -14,35 +14,3 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
-
-
-// валидация формы
-var form = document.querySelector('.form__form');
-var phone = form.querySelector('.form__phone');
-var email = form.querySelector('.form__email');
-var validity = form.querySelector('.form__validity')
-
-// phone.addEventListener('click", function (evt) {
-
-// }
-
-// );
-
-form.addEventListener("submit", function (evt) {
-  if (!phone.value || !email.value) {
-    evt.preventDefault();
-    validity.classList.add('form__validity--invalid');
-
-    // console.log("в штате йова все хуёва");
-
-    // popup.classList.remove("modal-error");
-    // popup.offsetWidth = popup.offsetWidth; // Хак. чтобы анимация ошибки отрабатывала несколько раз, если форма не валидна.
-    // popup.classList.add("modal-error");
-    // } else {
-    //   if (isStorageSupport) {
-    //     localStorage.setItem("username", username.value);
-    //   //   localStorage.setItem("email", email.value);
-    //   }
-
-  }
-});
